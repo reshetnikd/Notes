25239da8eb59b8bb3ff92aeae93c74172ed9cf69
reshetnikd <dmitry.reshetnik@icloud.com>
2020-05-14 21:53:44 +0300

    Extend String with some new features

--- Notes/NotesTableViewController.swift
+++ Notes/NotesTableViewController.swift
@@ -121,3 +121,36 @@ class NotesTableViewController: UITableViewController {
     }
 
 }
+
+extension String {
+    var capitalizedFirst: String {
+        guard let firstLetter = self.first else { return "" }
+        return firstLetter.uppercased() + self.dropFirst()
+    }
+    
+    subscript(i: Int) -> String {
+        return String(self[index(startIndex, offsetBy: i)])
+    }
+    
+    // remove a prefix if it exists
+    func deletingPrefix(_ prefix: String) -> String {
+        guard self.hasPrefix(prefix) else { return self }
+        return String(self.dropFirst(prefix.count))
+    }
+    
+    // remove a suffix if it exists
+    func deletingSuffix(_ suffix: String) -> String {
+        guard self.hasSuffix(suffix) else { return self }
+        return String(self.dropLast(suffix.count))
+    }
+    
+    func containsAny(of array: [String]) -> Bool {
+        for item in array {
+            if self.contains(item) {
+                return true
+            }
+        }
+        
+        return false
+    }
+}
